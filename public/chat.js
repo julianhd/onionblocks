b1dfdf2a3652000ca314399b384cfa806165015b
Julian Hoang <onionblocks@julianhoang.com>
2018-03-27 17:20:03 -0400

    Add checks for login/register failure

--- public/chat.js
+++ public/chat.js
@@ -3,21 +3,36 @@ const CHAT_SERVER_URL = "localhost:8081"
 window.chat = {
 	async register(name) {
 		const encoded = encodeURIComponent(name)
-		await fetch(`http://${CHAT_SERVER_URL}/register?name=${encoded}`, {
-			method: "POST",
-		})
+		const res = await fetch(
+			`http://${CHAT_SERVER_URL}/register?name=${encoded}`,
+			{
+				method: "POST",
+			},
+		)
+		if (res.status === 401) {
+			throw new Error("Unauthorized")
+		}
 	},
 	async login(name) {
 		const encoded = encodeURIComponent(name)
-		await fetch(`http://${CHAT_SERVER_URL}/login?name=${encoded}`, {
+		const res = await fetch(`http://${CHAT_SERVER_URL}/login?name=${encoded}`, {
 			method: "POST",
 		})
+		if (res.status === 401) {
+			throw new Error("Unauthorized")
+		}
 	},
 	async sendChat(message) {
 		const encoded = encodeURIComponent(message)
-		await fetch(`http://${CHAT_SERVER_URL}/chat?message=${encoded}`, {
-			method: "POST",
-		})
+		const res = await fetch(
+			`http://${CHAT_SERVER_URL}/chat?message=${encoded}`,
+			{
+				method: "POST",
+			},
+		)
+		if (res.status === 401) {
+			throw new Error("Unauthorized")
+		}
 	},
 	onMessage(callback) {
 		const websocket = new WebSocket(`ws://${CHAT_SERVER_URL}`)
