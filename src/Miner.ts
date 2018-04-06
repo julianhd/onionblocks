d03ff5139ebfcad2fd77d73792f741d1ca79fe5f
Julian Hoang <onionblocks@julianhoang.com>
2018-04-06 15:05:44 -0400

    Fix miner

--- src/Miner.ts
+++ src/Miner.ts
@@ -1,42 +1,44 @@
 import http from "http"
 import express from "express"
 import Blockchain, { BlockData, Block, BlockContent } from "./Blockchain"
-import { createHash } from "crypto";
+import { createHash } from "crypto"
 
-const app = express();
+const app = express()
 
 class Miner {
-    blockdata: BlockData<BlockContent>;
+	async mine(content: BlockContent) {
+		// get the current blockchain
+		const blockchain = new Blockchain(null) // will have Verifier once it is created
+		const blocks = await blockchain.get()
 
-    async mine(BlockContent) {
-        // get the current blockchain
-        const blockchain = new Blockchain(null);        // will have Verifier once it is created
-        const block = await blockchain.get();
-        
-        // create a new block data
-        const new_block_data: BlockData<BlockContent> = {
-            sequence: block[0].data.sequence + 1,       // set the sequence field > 1 than the previous one
-            nonce: 0,
-            previous: block[1].hash,
-            content: BlockContent
-        }
-        var valid_hash = false;
-        var miner_hash;
-        // mine a new hash
-        do {
-            miner_hash = createHash("sha256")
-            const miner_serialization = JSON.stringify(new_block_data)
-            miner_hash.update(miner_serialization)
-            const digest = miner_hash.digest("hex")
-            if(digest.substr(0,2) == '000') {
-                valid_hash = true;
-            }
-        } while(!valid_hash)
-        
-        const new_block: Block<BlockContent> = {
-            data: new_block_data,
-            hash: miner_hash
-        }
-        return new_block;
-    }
+		const previous_block = blocks[blocks.length - 1]
+
+		// create a new block data
+		const new_block_data: BlockData<BlockContent> = {
+			sequence: previous_block.data.sequence + 1, // set the sequence field > 1 than the previous one
+			nonce: 0,
+			previous: previous_block.hash,
+			signature: "", // TODO FIX
+			content: content,
+		}
+		var valid_hash = false
+		var miner_hash
+		const miner_serialization = JSON.stringify(new_block_data)
+		// mine a new hash
+		do {
+			miner_hash = createHash("sha256")
+			miner_hash.update(miner_serialization)
+			const digest = miner_hash.digest("hex")
+			if (digest.substr(0, 2) == "000") {
+				valid_hash = true
+			}
+			new_block_data.nonce++
+		} while (!valid_hash)
+
+		const new_block: Block<BlockContent> = {
+			data: new_block_data,
+			hash: miner_hash.digest("hex"),
+		}
+		return new_block
+	}
 }
