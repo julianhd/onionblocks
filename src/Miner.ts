8886bdf33d23930c4eafc947584567dabfa33e8a
SomeMatthew <someMathew@outlook.com>
2018-04-06 22:43:12 -0400

    Fixed issue when the chain was initially empty and miner not processing correctly.
    Fixed sequence_num to start at 0 if no blocks in the chain
    Fixed to have previous of the hashed blockdata if no previous block in the chain.
    Digest wasn't properly looked for 3 zero, was instead looking for "00" = "000"
    Reduce the correct nonce to the previous value when done mining.

--- src/Miner.ts
+++ src/Miner.ts
@@ -15,34 +15,42 @@ export default class Miner {
 		// get the current blockchain
 		const blockchain = new Blockchain(null) // will have Verifier once it is created
 		const blocks = await blockchain.get()
-
+		// console.log("Miner: blocks -- " + JSON.stringify(blocks));
 		const previous_block = blocks[blocks.length - 1]
+
+		var sequence_num = 0;
+		if (previous_block) {
+			sequence_num = previous_block.data.sequence + 1;
+		}
 
 		// create a new block data
 		const new_block_data: BlockData<BlockContent> = {
-			sequence: previous_block.data.sequence + 1, // set the sequence field > 1 than the previous one
+			sequence: sequence_num, // set the sequence field > 1 than the previous one
 			nonce: 0,
-			previous: previous_block.hash,
+			previous: (previous_block) ? previous_block.hash : null,
 			signature: entity.signature, // TODO FIX
 			content: entity.content,
 		}
 		var valid_hash = false
 		var miner_hash
-		const miner_serialization = JSON.stringify(new_block_data)
-		// mine a new hash
+		let digest;
 		do {
+			let miner_serialization = JSON.stringify(new_block_data)
+
+			// mine a new hash
 			miner_hash = createHash("sha256")
 			miner_hash.update(miner_serialization)
-			const digest = miner_hash.digest("hex")
-			if (digest.substr(0, 2) == "000") {
+			digest = miner_hash.digest("hex")
+			if (digest.substr(0, 3) == "000") {
 				valid_hash = true
 			}
 			new_block_data.nonce++
 		} while (!valid_hash)
+		new_block_data.nonce--
 
 		const new_block: Block<BlockContent> = {
 			data: new_block_data,
-			hash: miner_hash.digest("hex"),
+			hash: digest,
 		}
 		return new_block
 	}
