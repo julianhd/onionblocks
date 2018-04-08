ad0cf0e2c95be22fb5ed395ed16f1b7711232735
Amal <amal.zemm@gmail.com>
2018-04-08 00:28:08 -0400

    Most of the UI is ok now, I'll finish the rest tomorow. Also I added a password field in both register and login pages.

---
+++ public/js/login.js
@@ -0,0 +1,40 @@
+async function doLogin(username) {
+	try {
+		await chat.login(username)
+		document.cookie = "loggedIn=true; max-age:60"
+		var x = document.cookie
+		console.log(x)
+	} catch (error) {
+		// Login error occurred
+		console.error("Couldn't sign in!")
+		window.alert("There was a login error, please try again.")
+	}
+}
+
+function checkLogin() {
+	var loggedIn = getCookie("loggedIn")
+	console.log(loggedIn)
+	if (loggedIn == "true") {
+		console.log("Ya did it")
+		return true
+	} else {
+		console.log("Not logged in")
+	}
+	return false
+}
+
+function getCookie(cname) {
+	var name = cname + "="
+	var decodedCookie = decodeURIComponent(document.cookie)
+	var ca = decodedCookie.split(";")
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i]
+		while (c.charAt(0) == " ") {
+			c = c.substring(1)
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length)
+		}
+	}
+	return ""
+}
