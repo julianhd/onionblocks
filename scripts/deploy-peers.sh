5f043e7ea0c47431d9e54a809aae1f15ec1bb8a1
Julian Hoang <onionblocks@julianhoang.com>
2018-04-12 17:11:42 -0400

    Add deploy scripts

---
+++ scripts/deploy-peers.sh
@@ -0,0 +1,4 @@
+gcloud compute instances create onionblocks-2 --tags=http-server --metadata-from-file startup-script=scripts/gcloud-instance-startup.sh
+gcloud compute instances create onionblocks-3 --tags=http-server --metadata-from-file startup-script=scripts/gcloud-instance-startup.sh
+gcloud compute instances create onionblocks-4 --tags=http-server --metadata-from-file startup-script=scripts/gcloud-instance-startup.sh
+gcloud compute instances create onionblocks-5 --tags=http-server --metadata-from-file startup-script=scripts/gcloud-instance-startup.sh
